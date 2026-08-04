255a70e9d847dd6cffbd0c4221af137947b0888e
Houssam Allaoui <hsmallaouiwork@gmail.com>
2026-08-04 16:23:47 +0200

    correction d'erreur de lecture de ligne  et ajout de recherche de ligne

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pdfplumber
 pdf_path = "pdf/shift_test.pdf"
 
 with pdfplumber.open(pdf_path) as pdf:
-    for page in pdf.pages:
-        texte = page.extract_text()
-        print(texte)
+    texte = pdf.pages[0].extract_text()
+
+lignes = texte.split("\n")
+
+for ligne in lignes:
+    if ligne.startswith("Kiln 1"):
+        print("FOUND:", ligne)
