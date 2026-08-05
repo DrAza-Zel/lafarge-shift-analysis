d77f66a31c1ab33651ca35bc9d69c70bec140575
Houssam Allaoui <hsmallaouiwork@gmail.com>
2026-08-05 15:00:13 +0200

    les données extraites sont enregistrées  dans SQLITE

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from src.extract_pdf import extraire_shift
-from src.database import initialiser_base
+from src.database import initialiser_base, enregistrer_shift
 from pprint import pprint
 
 
 pdf_path = "pdf/shift_test.pdf"
 
-# Créer / initialiser la base de données
+# Créer la base et les tables si nécessaire
 initialiser_base()
 
 # Extraire le PDF
 shift = extraire_shift(pdf_path)
 
+# Afficher les données extraites
 pprint(shift, sort_dicts=False)
+
+# Enregistrer les données dans SQLite
+enregistrer_shift(shift)
--- src/database.py
+++ src/database.py
@@ -7,7 +7,7 @@ DB_PATH = "database/shifts.db"
 
 def initialiser_base():
 
-    # Créer le dossier database 
+    # Créer le dossier database
     os.makedirs("database", exist_ok=True)
 
     # Connexion à la base SQLite
@@ -29,7 +29,6 @@ def initialiser_base():
             responsable TEXT
         )
     """)
-
 
 
     # Table des mesures
@@ -54,3 +53,175 @@ def initialiser_base():
 
     # Fermer la connexion
     connexion.close()
+
+
+def enregistrer_shift(shift):
+
+    # Connexion à la base SQLite
+    connexion = sqlite3.connect(DB_PATH)
+
+    # Activer les clés étrangères
+    connexion.execute("PRAGMA foreign_keys = ON")
+
+    # Création d'un curseur
+    curseur = connexion.cursor()
+
+
+    
+    # Enregistrer le shift
+    curseur.execute("""
+        INSERT INTO shifts (
+            date_debut,
+            heure_debut,
+            date_fin,
+            heure_fin,
+            poste,
+            responsable
+        )
+        VALUES (?, ?, ?, ?, ?, ?)
+    """, (
+        shift["date_debut"],
+        shift["heure_debut"],
+        shift["date_fin"],
+        shift["heure_fin"],
+        shift["poste"],
+        shift["responsable"]
+    ))
+
+
+    # Récupérer l'id du shift qui vient d'être créé
+    shift_id = curseur.lastrowid
+
+
+   
+    # Enregistrer les données Cuisson
+    for equipement in shift["cuisson"]:
+
+        nom_equipement = equipement["equipement"]
+
+        for kpi, valeur in equipement.items():
+
+            if kpi == "equipement":
+                continue
+
+            curseur.execute("""
+                INSERT INTO mesures (
+                    shift_id,
+                    section,
+                    equipement,
+                    produit,
+                    kpi,
+                    valeur
+                )
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (
+                shift_id,
+                "cuisson",
+                nom_equipement,
+                None,
+                kpi,
+                valeur
+            ))
+
+
+
+    # Enregistrer les broyeurs ciment
+    for broyeur in shift["broyeurs"]:
+
+        nom_broyeur = broyeur["broyeur"]
+        produit = broyeur["produit"]
+
+        for kpi, valeur in broyeur.items():
+
+            if kpi == "broyeur" or kpi == "produit":
+                continue
+
+            curseur.execute("""
+                INSERT INTO mesures (
+                    shift_id,
+                    section,
+                    equipement,
+                    produit,
+                    kpi,
+                    valeur
+                )
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (
+                shift_id,
+                "broyeurs",
+                nom_broyeur,
+                produit,
+                kpi,
+                valeur
+            ))
+
+
+   
+    # Enregistrer les données environnement
+    for equipement in shift["environnement"]:
+
+        nom_equipement = equipement["equipement"]
+
+        for kpi, valeur in equipement.items():
+
+            if kpi == "equipement":
+                continue
+
+            curseur.execute("""
+                INSERT INTO mesures (
+                    shift_id,
+                    section,
+                    equipement,
+                    produit,
+                    kpi,
+                    valeur
+                )
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (
+                shift_id,
+                "environnement",
+                nom_equipement,
+                None,
+                kpi,
+                valeur
+            ))
+
+
+    
+    # Enregistrer les données compresseurs
+    for equipement in shift["compresseurs"]:
+
+        nom_equipement = equipement["equipement"]
+
+        for kpi, valeur in equipement.items():
+
+            if kpi == "equipement":
+                continue
+
+            curseur.execute("""
+                INSERT INTO mesures (
+                    shift_id,
+                    section,
+                    equipement,
+                    produit,
+                    kpi,
+                    valeur
+                )
+                VALUES (?, ?, ?, ?, ?, ?)
+            """, (
+                shift_id,
+                "compresseurs",
+                nom_equipement,
+                None,
+                kpi,
+                valeur
+            ))
+
+
+    # Sauvegarder les données
+    connexion.commit()
+
+    # Fermer la connexion
+    connexion.close()
+
+    print("Shift enregistré avec l'id :", shift_id)
