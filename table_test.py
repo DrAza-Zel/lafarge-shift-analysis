e6e71dcaffff5f1ce861455cbd3a582a946f5600
Houssam Allaoui <hsmallaouiwork@gmail.com>
2026-08-05 13:19:57 +0200

    Extraction des entetes du tableau et resolution des kpi dupliqués non organisés

---
+++ table_test.py
@@ -0,0 +1,78 @@
+import pdfplumber
+from pprint import pprint
+
+pdf_path = "pdf/shift_test.pdf"
+
+
+def nettoyer(texte):
+    if texte is None:
+        return None
+
+    return " ".join(texte.split())
+
+
+with pdfplumber.open(pdf_path) as pdf:
+    page = pdf.pages[0]
+    table = page.extract_table()
+
+
+# Chercher la ligne d'en-tête Cuisson
+indice_cuisson = None
+
+for numero, ligne in enumerate(table):
+
+    if ligne and nettoyer(ligne[0]) == "Cuisson":
+        indice_cuisson = numero
+        break
+
+
+# Récupérer les noms des colonnes
+entetes = table[indice_cuisson]
+
+
+# Chercher Kiln 1
+ligne_kiln1 = None
+
+for ligne in table[indice_cuisson + 1:]:
+
+    if ligne and nettoyer(ligne[0]) == "Kiln 1":
+        ligne_kiln1 = ligne
+        break
+
+
+# Créer le dictionnaire
+kiln1 = {
+    "equipement": nettoyer(ligne_kiln1[0])
+}
+
+
+# Ce dictionnaire sert à compter
+# combien de fois chaque en-tête apparaît
+compteur_entetes = {}
+
+
+for entete, valeur in zip(entetes[1:], ligne_kiln1[1:]):
+
+    entete = nettoyer(entete)
+    valeur = nettoyer(valeur)
+
+    if entete is not None and valeur is not None and valeur != "":
+
+        # Compter combien de fois cet en-tête a déjà été rencontré
+        if entete not in compteur_entetes:
+            compteur_entetes[entete] = 1
+        else:
+            compteur_entetes[entete] += 1
+
+        # Premier en-tête : nom normal
+        # En-têtes suivants : _2, _3, ...
+        if compteur_entetes[entete] == 1:
+            nom_entete = entete
+        else:
+            nom_entete = entete + "_" + str(compteur_entetes[entete])
+
+        # Ajouter la valeur dans le dictionnaire final
+        kiln1[nom_entete] = float(valeur)
+
+
+pprint(kiln1, sort_dicts=False)
